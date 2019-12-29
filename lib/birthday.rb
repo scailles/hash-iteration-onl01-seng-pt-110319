72692e1f0cb860ba7e18624cc4224bad1ab6744e
Shaina C <scailles23@gmail.com>
2019-12-29 07:24:50 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,11 @@
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |kids_name,age|
+    if age<=18
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  else
+    puts "You are too old for this"
+  end
   end
 end
 
