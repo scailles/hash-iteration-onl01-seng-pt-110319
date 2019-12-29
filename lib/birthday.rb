c4dc520304f24f68b83c6d343828f23a4e741ebd
Shaina C <scailles23@gmail.com>
2019-12-29 07:44:36 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,3 +7,6 @@
 def happy_birthday(birthday_kids)
   birthday_kids.each {|kids_name,age| puts "Happy Birthday #{kids_name}! You are now #{age} years old!"}
 end
+
+def age_appropriate_birthday(birthday_kids)
+  
