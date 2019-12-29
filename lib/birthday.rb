1f195e520bc8081261f3c746b28e4b186587e60b
Shaina C <scailles23@gmail.com>
2019-12-29 07:38:27 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,6 +8,5 @@ def happy_birthday(birthday_kids)
   birthday_kids.each {|kids_name,age| puts (case when age<18 "Happy Birthday #{kids_name}! You are now #{age} years old!"
   when age>=18 "You are too old for this")}
   end
-end
-end
+
 
